77b117f9ddeb912b851c8e50c670197a1bb2a3ed
Malik .. <p240554@pwr.nu.edu.pk>
2026-08-20 23:20:57 +0500

    Update problem-5.js

---
+++ Task-7/Problems/problem-5.js
@@ -0,0 +1,6 @@
+let weight=Number(prompt("Enter your weight in kg:"));
+let height=Number(prompt("Enter your height in meters:"));
+
+let bmi=weight/(height*height);
+
+alert("Your BMI is: " + bmi);
